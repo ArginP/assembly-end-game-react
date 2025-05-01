8a4deeeff191cc7a3cdd036e985adb24229e8c4e
ArginP <n7toman@gmail.com>
2025-05-01 15:20:20 +0300

    feat: add Keyboard component & styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header'
 import { GameStatus } from './components/GameStatus'
 import { HealthBar } from './components/HealthBar'
 import { WordDisplay } from './components/WordDisplay'
+import { Keyboard } from './components/Keyboard'
 
 export type GameStatusState = 'inProgress' | 'isWon' | 'isLost'
 
@@ -21,6 +22,7 @@ function App() {
         <WordDisplay
           currentWord={currentWord}
         />
+        <Keyboard />
       </main>
     </>
   )
---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,16 @@
+export const Keyboard = () => {
+  const alphabet = 'а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я'
+  const currentWordArray = alphabet.split(" ");
+
+  return (
+    <section className="keyboard">
+      {currentWordArray.map((letter) => {
+        return (
+          <button key={letter} className="key" style={{}}>
+            {letter}
+          </button>
+        );
+      })}
+    </section>
+  );
+};
